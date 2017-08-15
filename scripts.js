7c56bf4a492ccc78f05871f49ef4a2a4ba5ed179
Kelly Miller <kellyannemiller@gmail.com>
2017-08-15 11:11:58 -0600

    clean code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	// Set the interval to be 5 seconds
 	var t = setInterval(function(){
 		$("#carousel ul").animate({marginLeft:-480},1000,function(){
 			$(this).find("li:last").after($(this).find("li:first"));
